cabafb9941ee1e8c1cd9ee5daeef133bae7e72dd
estevaolelis <estevaolelis@gmail.com>
2025-12-09 17:36:02 -0300

    fix: criação de endpoint para conta

---
+++ ApiFinanceira/Controllers/ContaController.cs
@@ -0,0 +1,24 @@
+﻿using ApiFinanceira.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiFinanceira.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ContaController : ControllerBase
+{
+    private readonly ContaService _contaService;
+
+    public ContaController(ContaService contaService)
+    {
+        _contaService = contaService;
+    }
+    
+    [HttpGet("listar-conta")]
+    public async Task<IActionResult> ListarConta()
+    {
+        var listar = await _contaService.GetContasAsync();
+        
+        return Ok(listar);
+    }
+}
---
+++ ApiFinanceira/Services/ContaService.cs
@@ -0,0 +1,18 @@
+﻿namespace ApiFinanceira.Services;
+
+public class ContaService
+{
+    private readonly Supabase.Client _supabase;
+
+    public ContaService(Supabase.Client supabase)
+    {
+        _supabase = supabase;
+    }
+    
+    public async Task<List<conta>> GetContasAsync()
+    {
+        var response = await _supabase.From<conta>().Get();
+
+        return response.Models;
+    }
+}
