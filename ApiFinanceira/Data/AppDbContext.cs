5a3c6434477caaec52d36f0324d18dfbff368b4e
estevaolelis <estevaolelis@gmail.com>
2026-01-31 15:48:03 -0300

    feat: ajuste para ter um appsettings de exemplo

--- ApiFinanceira/Data/AppDbContext.cs
+++ ApiFinanceira/Data/AppDbContext.cs
@@ -5,7 +5,9 @@ namespace ApiFinanceira.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
 
         public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Conta> Contas { get; set; }
